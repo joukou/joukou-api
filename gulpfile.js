e889581e8a1ca3674640d33add544b1daa98d04d
Isaac Johnston <isaac.johnston@joukou.co>
2014-04-17 15:26:59 +1200

    Removes gulpfile and dependencies

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Register the coffee-script compiler for use in the gulpfile.
- *
- * @author Isaac Johnston <isaac.johnston@joukou.co>
- * @copyright 2014 Joukou Ltd. All rights reserved.
- */
-
-require('coffee-script/register');
-require('./gulpfile.coffee');
