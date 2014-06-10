38a3f58ce79a95f7458fa684edeff3d3d421af1d
Isaac Johnston <isaac.johnston@joukou.co>
2014-06-10 21:40:11 +1200

    Introduces sleep to Riak setup process

--- setup.sh
+++ setup.sh
@@ -7,11 +7,15 @@
 # when ready.
 
 curl -XPUT http://localhost:8098/search/schema/agent -H'content-type:application/xml' --data-binary @dist/agent/schema.xml
+sleep 5
 curl -XPUT http://localhost:8098/search/index/agent -H'content-type:application/json' -d'{"schema":"agent"}'
-sudo riak-admin bucket-type create agent '{"props":{"search_index":"agent","allow_mult":false}}'
-sudo riak-admin bucket-type activate agent
+sleep 5
+riak-admin bucket-type create agent '{"props":{"search_index":"agent","allow_mult":false}}'
+riak-admin bucket-type activate agent
 
 curl -XPUT http://localhost:8098/search/schema/persona -H'content-type:application/xml' --data-binary @dist/persona/schema.xml
+sleep 5
 curl -XPUT http://localhost:8098/search/index/persona -H'content-type:application/json' -d'{"schema":"persona"}'
-sudo riak-admin bucket-type create persona '{"props":{"search_index":"persona","allow_mult":false}}'
-sudo riak-admin bucket-type activate persona
+sleep 5
+riak-admin bucket-type create persona '{"props":{"search_index":"persona","allow_mult":false}}'
+riak-admin bucket-type activate persona
