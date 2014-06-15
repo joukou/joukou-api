68d15a19b18ac7af877d9bce0998d34787605e3f
Isaac Johnston <isaac.johnston@joukou.co>
2014-06-15 12:49:47 +1200

    Re-enables tests

--- .circle/riak.sh
+++ .circle/riak.sh
@@ -3,19 +3,27 @@
 # Author: Isaac Johnston <isaac.johnston@joukou.com>
 # Copyright: (c) 2009-2014 Joukou Ltd. All rights reserved.
 #
-# Temporary Basho Riak 2.0 setup script. To be replaced by CoffeeScript version
-# when ready.
+# Configuration and initialization of Basho Riak 2.0-beta1
+
+if [ "$CIRCLECI" ]
+then
+  rm /etc/riak/app.config
+  cp ~/joukou-api/.circle/riak.conf /etc/riak/riak.conf
+  cp ~/joukou-api/.circle/env.sh /usr/lib/riak/lib/env.sh
+  sudo riak start
+  sleep 2
+fi
 
 curl -XPUT http://localhost:8098/search/schema/agent -H'content-type:application/xml' --data-binary @dist/agent/schema.xml
-sleep 5
 curl -XPUT http://localhost:8098/search/index/agent -H'content-type:application/json' -d'{"schema":"agent"}'
 sleep 5
 sudo riak-admin bucket-type create agent '{"props":{"search_index":"agent","allow_mult":false}}'
 sudo riak-admin bucket-type activate agent
 
 curl -XPUT http://localhost:8098/search/schema/persona -H'content-type:application/xml' --data-binary @dist/persona/schema.xml
-sleep 5
 curl -XPUT http://localhost:8098/search/index/persona -H'content-type:application/json' -d'{"schema":"persona"}'
 sleep 5
 sudo riak-admin bucket-type create persona '{"props":{"search_index":"persona","allow_mult":false}}'
 sudo riak-admin bucket-type activate persona
+
+sleep 2
