59d8447b87768cab150affdb7f5c8523163c8612
Isaac Johnston <isaac.johnston@joukou.co>
2014-06-24 22:11:26 +1200

    Adds skeleton for re-introduction of hyper/* modules

---
+++ dist/hyper/Link.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+@class joukou-api/hyper/Link
+@author Isaac Johnston <isaac.johnston@joukou.com>
+@copyright &copy; 2009-2014 Joukou Ltd. All rights reserved.
+ */
+module.exports = (function() {
+  function _Class(_arg) {
+    this.rel = _arg.rel, this.href = _arg.href, this.templated = _arg.templated, this.type = _arg.type, this.name = _arg.name, this.profile = _arg.profile, this.title = _arg.title, this.hreflang = _arg.hreflang;
+  }
+
+  return _Class;
+
+})();
+
+/*
+//# sourceMappingURL=Link.js.map
+*/
---
+++ dist/hyper/Schema.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+@class joukou-api/hyper/Schema
+@author Isaac Johnston <isaac.johnston@joukou.com>
+@copyright &copy; 2009-2014 Joukou Ltd. All rights reserved.
+ */
+module.exports = (function() {
+  function _Class(schema) {
+    this.schema = schema;
+  }
+
+  return _Class;
+
+})();
+
+/*
+//# sourceMappingURL=Schema.js.map
+*/
