570642e43b0a699f5122521be501593a074d0123
Isaac Johnston <isaac.johnston@joukou.com>
2014-10-06 16:15:17 +1300

    feat(bin/riak-setup.sh): add riak setup script for OS X

---
+++ bin/riak-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Author: Isaac Johnston <isaac.johnston@joukou.com>
+# Copyright: (c) 2009-2014 Joukou Ltd. All rights reserved.
+#
+# Configuration and initialization of Basho Riak 2.0-beta1
+
+curl -XPUT http://localhost:8098/search/schema/agent -H'content-type:application/xml' --data-binary @dist/agent/schema.xml
+curl -XPUT http://localhost:8098/search/index/agent -H'content-type:application/json' -d'{"schema":"agent"}'
+sleep 10
+riak-admin bucket-type create agent '{"props":{"search_index":"agent","allow_mult":false}}'
+riak-admin bucket-type activate agent
+
+curl -XPUT http://localhost:8098/search/schema/persona -H'content-type:application/xml' --data-binary @dist/persona/schema.xml
+curl -XPUT http://localhost:8098/search/index/persona -H'content-type:application/json' -d'{"schema":"persona"}'
+sleep 10
+riak-admin bucket-type create persona '{"props":{"search_index":"persona","allow_mult":false}}'
+riak-admin bucket-type activate persona
+
+curl -XPUT http://localhost:8098/search/schema/graph -H'content-type:application/xml' --data-binary @dist/persona/graph/schema.xml
+curl -XPUT http://localhost:8098/search/index/graph -H'content-type:application/json' -d'{"schema":"graph"}'
+sleep 10
+riak-admin bucket-type create graph '{"props":{"search_index":"graph","allow_mult":false}}'
+riak-admin bucket-type activate graph
+
+sleep 2
